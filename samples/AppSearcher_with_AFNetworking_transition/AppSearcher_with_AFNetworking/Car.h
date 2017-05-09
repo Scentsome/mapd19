bd8950d95d4c195fa854f08e1408b8c6e56b76a3
Michael Pan <scentsome@gmail.com>
2017-05-09 14:52:59 +0800

    Feat: Testing Data and Equality

---
+++ samples/AppSearcher_with_AFNetworking_transition/AppSearcher_with_AFNetworking/Car.h
@@ -0,0 +1,13 @@
+//
+//  Car.h
+//  AppSearcher_with_AFNetworking
+//
+//  Created by Michael on 09/05/2017.
+//  Copyright © 2017 com.zencher.demo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+
+@end
