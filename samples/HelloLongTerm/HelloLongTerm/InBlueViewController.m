89da5f822b3580cadcf28a404e7b530f99e23d92
Michael Pan <scentsome@gmail.com>
2017-05-11 13:49:56 +0800

    Feat: Demo tableview reuse cell

--- samples/HelloLongTerm/HelloLongTerm/InBlueViewController.m
+++ samples/HelloLongTerm/HelloLongTerm/InBlueViewController.m
@@ -8,15 +8,18 @@
 
 #import "InBlueViewController.h"
 
-@interface InBlueViewController ()
+@interface InBlueViewController ()<UITableViewDataSource, UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *inbluelabel;
-
+@property NSArray * names ;
+@property NSMutableArray<NSNumber *> * checked ;
 @end
 
 @implementation InBlueViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.names = @[@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"11",@"12"];
+    self.checked = [@[@NO,@NO,@NO,@NO,@NO,@NO,@NO,@NO,@NO,@NO,@NO,@NO] mutableCopy];
     // Do any additional setup after loading the view.
 }
 
@@ -27,6 +30,40 @@
 
 -(void) sendMessage:(NSString *)blueMessage{
     self.inbluelabel.text = blueMessage;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.names.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    
+    if(cell == nil){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+    }
+    cell.textLabel.text = self.names[indexPath.row];
+    [self configureCell:cell withIndexPath:indexPath ];
+    return cell;
+}
+-(void) configureCell:(UITableViewCell *) theCell withIndexPath:(NSIndexPath *) theIndexPath{
+    if(self.checked[theIndexPath.row].boolValue){
+        theCell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }else{
+        theCell.accessoryType = UITableViewCellAccessoryNone;
+    }
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell * selectedCell = [tableView cellForRowAtIndexPath:indexPath];
+    if(self.checked[indexPath.row].boolValue){
+        selectedCell.accessoryType = UITableViewCellAccessoryNone;
+        self.checked[indexPath.row] = @NO;
+    }else{
+        selectedCell.accessoryType = UITableViewCellAccessoryCheckmark;
+        self.checked[indexPath.row] = @YES;
+    }
+    
+    
 }
 
 /*
