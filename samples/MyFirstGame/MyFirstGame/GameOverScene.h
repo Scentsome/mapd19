bc8a92a63d6e25d28966f4657f95bb79cf4f61ce
Michael Pan <scentsome@gmail.com>
2017-05-16 15:21:32 +0800

    Feat: Add game

---
+++ samples/MyFirstGame/MyFirstGame/GameOverScene.h
@@ -0,0 +1,13 @@
+//
+//  GameOverScene.h
+//  MyFirstGame
+//
+//  Created by Michael on 16/05/2017.
+//  Copyright © 2017 Zencher. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameOverScene : SKScene
+
+@end
---
+++ samples/MyFirstGame/MyFirstGame/GameOverScene.m
@@ -0,0 +1,13 @@
+//
+//  GameOverScene.m
+//  MyFirstGame
+//
+//  Created by Michael on 16/05/2017.
+//  Copyright © 2017 Zencher. All rights reserved.
+//
+
+#import "GameOverScene.h"
+
+@implementation GameOverScene
+
+@end
